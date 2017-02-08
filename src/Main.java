82dafcd1364e3981c8d6c3003b6b685d5339b998
kevin.b.bridges <kevin.b.bridges@gmail.com>
2017-02-08 17:35:29 -0500

    Completed problem 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeSet;
 
 public class Main {
@@ -19,7 +21,8 @@ public class Main {
 		Shape triangle = new Triangle(5, 6, 7);
 		System.out.println("Triangle Area: "+triangle.getArea());
 		System.out.println("Triangle Perimeter: "+triangle.getPerimeter());
-		
+		System.out.println("-----------------------------------------------------");
+		System.out.println("");
 		
 		// Define 2 collections
 		List<Integer> ListOne = RandomIntList.ThisRandomIntList(10, 1);
@@ -36,7 +39,6 @@ public class Main {
 		
 		//Convert to TreeSet for Sorting Purpose
 		TreeSet<Integer> TSCombinedList= HashToTree.Convert(MyCombinedIntList);
-		System.out.println("------------------------------");
 		System.out.println("TreeSet contains: " + TSCombinedList.toString());
 		
 		//Remove Middle Element
@@ -52,7 +54,24 @@ public class Main {
 		
 		
 		//Define a collection that contains a key/value pair
-		//TODO
+		//•	Add 5 key/value pairs to the collection
+		//
+		// I'm using a LinkedHashMap because it doesn't allow duplicates
+		// and it is sort-able
+		//
+		// Map representing (Subject, grades) as (key, value) pair
+		Map<String, Double> linkedHashMap = new LinkedHashMap<String, Double>();
+		linkedHashMap.put("History", new Double(97.98));
+		linkedHashMap.put("Science", new Double(89.76));
+		linkedHashMap.put("Math", new Double(90.47));
+		linkedHashMap.put("PE", new Double(95.00));
+		linkedHashMap.put("English", new Double(81.55));
+		linkedHashMap.put("English", new Double(81.55));
+		
+		// Displaying the contents of the LinkedHashMap
+		System.out.println("Contents of LinkedHashMap : " + linkedHashMap);
+		System.out.println("Size of LinkedHashMap: " + linkedHashMap.size());
+
 	}
 
 
